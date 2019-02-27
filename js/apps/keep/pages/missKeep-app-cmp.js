83fa61999b710487c3250d799a7456efda126c53
Daniel Yosilevich <danielyosilevich@gmail.com>
2019-02-27 12:17:38 +0200

    update

--- js/apps/keep/pages/missKeep-app-cmp.js
+++ js/apps/keep/pages/missKeep-app-cmp.js
@@ -1,13 +1,49 @@
 export default {
     template: 
         `<section class="missKeep-app-body">
-            <h1>missKeep</h1  
-        </section>`
-    ,
+            <div class = "missKeep-wrapper">
+               <header>
+              <h1>missKeep</h1>  
+             <input type="text" placeholder="Search">
+             <hr>
+             </header>
+             <main class = "flex justify-center align-center">
+                <div class = "notes-container">
+                  <ul>
+                       <li> 
+                       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dolorum, deleniti voluptates assumenda saepe suscipit provident voluptatibus soluta aut totam vero recusandae, nemo et tenetur quas labore maxime. Itaque, quo.
+                       </li>
+                      <li> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dolorum, deleniti voluptates assumenda saepe suscipit provident voluptatibus soluta aut totam vero recusandae, nemo et tenetur quas labore maxime. Itaque, quo.
+                      </li>
+                      <li> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dolorum, deleniti voluptates assumenda saepe suscipit provident voluptatibus soluta aut totam vero recusandae, nemo et tenetur quas labore maxime. Itaque, quo.
+                      </li>
+                 </ul>
+                 </div>
+                 <div class = "img-container">
+                  <ul>
+                       <li> 
+                       <img src = "../img/Clubs-Ace.jpg" > </img>
+                       </li>
+                      <li> 
+                      <img src = "../img/Clubs-Ace.jpg" > </img>
+                      </li>
+                      <li> 
+                      <img src = "../img/Clubs-Ace.jpg" > </img>
+                      </li>
+                 </ul>
+                 </div>
+             </main>
+            </div>
+        </section>
+    `,
+
     data() {
         return {
             
         }
+    },
+    components: {
+        
     },
     methods: {
         
@@ -16,9 +52,7 @@ export default {
        
     },
     created() {
-           
-    },
-    components: {
-        
+         console.log(`Miss Keep Page is loaded!`) ;
     }
+   
 }
