5fbeb33de64eab14616327283f62966a69eda8ea
Daniel Yosilevich <danielyosilevich@gmail.com>
2019-03-01 21:08:01 +0200

    update

--- js/apps/keep/cmps/keep-notes-cmp.js
+++ js/apps/keep/cmps/keep-notes-cmp.js
@@ -7,7 +7,7 @@ export default {
     components: {
         keepDisplayNote
     },
-    data(){
+    data() {
         return {
             notes: this.data
         }
@@ -16,21 +16,33 @@ export default {
         <section class = "keep-notes">
             Keep Notes Component <hr>
             <ul class = "flex align-center justify-center">
-            <div v-for="(currNote,index) in notes" :key = "currNote.id" class = "flex justify-center align-center">  
-               <router-link :to="{name: 'noteRoute', params: {id: currNote.id, content : currNote.content, index: index}}" >
-                    <keep-display-note
-                        :content = "currNote.content" :index = "index">
-                    </keep-display-note>
-                </router-link>
+            <div v-for="(currNote,index) in notes" :key = "currNote.id" 
+                     @click = "triggerNoteRoute(currNote,index)"
+                     class = "flex justify-center align-center">  
+                <keep-display-note
+                    :content = "currNote.content" :index = "index">
+                </keep-display-note>
             </div>    
-        </ul>
-        <router-view v-if="$route.fullPath === '/missKeep-app/note'"></router-view>
+            </ul>
+            <router-view v-if="$route.fullPath === '/missKeep-app/note'"></router-view>
+            
+            
+            </section>
+            `,
+    methods: {
+        triggerNoteRoute(note,index) {
+           // console.log(note.id);
+            this.$router.push({ name: 'noteRoute',params:{ id:note.id,content : note.content,index:index }});
 
-
-        </section>
-    `,
-    created(){
+        }
+    },
+    created() {
+        //    <router-link :to="{name: 'noteRoute', params: {id: currNote.id, content : currNote.content, index: index}}" >
+        //         <keep-display-note
+        //             :content = "currNote.content" :index = "index">
+        //         </keep-display-note>
+        //     </router-link>
         console.log('keepNotes linked');
-        console.log('notes',this.notes);
+        console.log('notes', this.notes);
     }
 }
