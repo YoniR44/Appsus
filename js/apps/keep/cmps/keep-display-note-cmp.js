fb2b0f22baedbc9557000ada00b4540bec3e280a
Daniel Yosilevich <danielyosilevich@gmail.com>
2019-03-02 15:16:46 +0200

    update

--- js/apps/keep/cmps/keep-display-note-cmp.js
+++ js/apps/keep/cmps/keep-display-note-cmp.js
@@ -1,10 +1,70 @@
 export default {
-    props:{
-        content: String
+    props: {
+        content: String,
+        isActive: Number,
+        index: Number,
+        background: String,
+        isOutside: Boolean,
+        currIndex: Number
+    },
+    data() {
+        return {
+            colors: [{ hex: 'black' },
+            { hex: 'green' },
+            { hex: 'red' }
+            ],
+            colors1: [{ hex: 'black' },
+            { hex: 'green' },
+            { hex: 'white' },
+            { hex: 'red' }
+            ],
+            colors2: [{ hex: 'yellow' },
+            { hex: 'magenta' },
+            { hex: 'cyan' },
+            { hex: 'orange' }
+            ],
+        }
     },
     template: `
-           <li>
-               <p> {{content}} </p>
-           </li>    
-        `,
-}   
+           <li class = "li-wrapper" :style="{background: background}" >
+           <div>
+               <button class = "btn-pin fa fa-map-pin"></button>
+               <div> 
+                     <p> {{content}} </p>
+               </div>
+               <div v-if = "toShowDiv">
+               <button class = "btn-remove fa fa-remove"></button> 
+               <button  v-show = "toShowBtn" class = "btn-palette fa fa-paint-brush">
+               </button>
+               <table class = "dropdown-table" v-show = "toShowTbl">
+                    <tbody>
+                    <tr>
+                        <td v-for="color in colors1" class = "dropdown-color"  
+                        :style="{background: color.hex}" 
+                        > &nbsp&nbsp&nbsp</td>
+                    </tr>
+                    <tr>
+                        <td v-for="color in colors2" class = "dropdown-color" 
+                        :style="{background: color.hex}" 
+                        > &nbsp&nbsp&nbsp</td>
+                    </tr>
+                    </tbody>
+                </table> 
+                </div>  
+            </div>  
+            </li>    
+    `,
+    computed: {
+        toShowTbl() {
+            return this.isActive === this.index;
+        },
+        toShowBtn() {
+            return !this.toShowTbl;
+        },
+        toShowDiv(){
+             return !this.isOutside && this.currIndex === this.index
+        }
+    },
+    created() {
+    }
+}
--- js/apps/keep/cmps/keep-notes-cmp.js
+++ js/apps/keep/cmps/keep-notes-cmp.js
@@ -9,30 +9,58 @@ export default {
     },
     data() {
         return {
-            notes: this.data
+            notes: this.data,
+            currActiveIndex: -1,
+            isOutside: true,
+            currIndex: -1
         }
     },
     template: `
         <section class = "keep-notes">
-            Keep Notes Component <hr>
             <ul class = "flex align-center justify-center">
-            <div v-for="(currNote,index) in notes" :key = "currNote.id" 
-                     @click = "triggerNoteRoute(currNote,index)"
-                     class = "flex justify-center align-center">  
+            <div v-for="(currNote,index) in notes" :key = "currNote.id"              
+                     @click = "onClick(currNote,index,$event)"
+                     @mouseleave = "onMouseLeave(index,$event)"
+                     @mouseenter = "onMouseEnter(index,$event)"
+                     class = "notes-layout-wrapper flex justify-center align-center">  
                 <keep-display-note
-                    :content = "currNote.content" :index = "index">
+                    :isActive = "currActiveIndex"
+                    :isOutside = "isOutside"
+                    :currIndex = "currIndex"
+                    :content = "currNote.content" :index = "index"
+                    :background = "currNote.bgnd">
                 </keep-display-note>
             </div>    
             </ul>
-            <router-view v-if="$route.fullPath === '/missKeep-app/note'"></router-view>
-            
-            
+            <router-view v-if="$route.fullPath === '/missKeep-app/note'"></router-view>             
             </section>
             `,
     methods: {
-        triggerNoteRoute(note,index) {
-            this.$router.push({ name: 'noteRoute',params:{ id:note.id,content : note.content,index:index }});
-
+        onClick(note, index, ev) {
+            console.log(ev.target.classList);
+            if (ev.target.tagName !== 'BUTTON' && ev.target.tagName !== 'TD') {
+                this.$router.push({ name: 'noteRoute', params: { id: note.id, content: note.content, index: index } });
+                return;
+            }
+            if (ev.target.classList.contains('btn-palette')) {
+                this.currActiveIndex = index;
+                return;
+            }
+            // console.log('clicked palette!',this.isActive);      
+            if (ev.target.tagName === 'TD') {
+                this.notes[index].bgnd = ev.target.style.backgroundColor;
+                this.currActiveIndex = -1;
+                return;
+            }
+        },
+        onMouseLeave() {
+            this.isOutside = true;
+            this.currActiveIndex = -1;
+            this.currIndex = -1;
+        },
+        onMouseEnter(index) {
+            this.isOutside = false;
+            this.currIndex = index;
         }
     },
     created() {
